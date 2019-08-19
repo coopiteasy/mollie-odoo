b35de545fb56e8d2a6ad579b08ac539e4ae71e81
Yenthe666 <yenthevg@gmail.com>
2019-08-19 02:57:49 -0700

    [ADD] payment_mollie_official: add option to (de)activate payment methods on Mollie.com from Odoo

--- payment_mollie_official/models/payment_icon.py
+++ payment_mollie_official/models/payment_icon.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, api, _
+from mollie.api.client import Client
+from odoo.addons.payment_mollie_official.models.mollie import get_mollie_provider_key
 
 
 class PaymentIcon(models.Model):
     _inherit = 'payment.icon'
 
-    active = fields.Boolean(string='Active', default=True)
+    _mollie_client = Client()
+
+    active = fields.Boolean(string='Active')
+
+    @api.multi
+    def toggle_active(self):
+        for record in self:
+            record.active = not record.active
+            if record.provider == 'mollie':
+                key = get_mollie_provider_key(self.env)
+                self._mollie_client.set_api_key(key)
+
+                if record.active:
+                    # Activates the payment method for your Mollie account (on mollie.com).
+                    # Note: this only works when the payment acquirer Mollie is in 'production' and not in test mode.
+                    self._mollie_client.profile_methods.with_parent_id('me', record.acquirer_reference).create()
+                else:
+                    # Dectivates the payment method for your Mollie account (on mollie.com).
+                    # Note: this only works when the payment acquirer Mollie is in 'production' and not in test mode.
+                    self._mollie_client.profile_methods.with_parent_id('me', record.acquirer_reference).delete()
